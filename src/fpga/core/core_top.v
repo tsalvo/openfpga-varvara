370eb4ee4f8233fd5e311c98372ef921c9c29ca5
Tom Salvo <thsalvo@icloud.com>
2024-07-06 22:22:26 -0700

    reduce clock speed to meet timing for submodule with dual port stack ram

--- src/fpga/core/core_top.v
+++ src/fpga/core/core_top.v
@@ -520,10 +520,10 @@ assign video_hs = vidout_hs;
 
     localparam  VID_V_BPORCH = 'd10;
     localparam  VID_V_ACTIVE = 'd256;
-    localparam  VID_V_TOTAL = 'd333;
+    localparam  VID_V_TOTAL = 'd400;
     localparam  VID_H_BPORCH = 'd10;
     localparam  VID_H_ACTIVE = 'd256;
-    localparam  VID_H_TOTAL = 'd1001;
+    localparam  VID_H_TOTAL = 'd800;
 
     reg [3:0] uxn_c_current_pixel_r;
     reg [3:0] uxn_c_current_pixel_g;
