628a53a5aa21ed28d8606ece1caedab59d14aba1
Tom Salvo <tsalvo@noreply.codeberg.org>
2025-09-02 14:16:51 -0700

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-~/altera_lite/24.1std/quartus/bin/quartus_sh --flow compile src/fpga/ap_core.qpf
+~/intelFPGA_lite/24.1std/quartus/bin/quartus_sh --flow compile ./src/fpga/ap_core.qpf
+rbfrmake ./src/fpga/output_files/ap_core.rbf ./src/fpga/output_files/bitstream.rbf_r
