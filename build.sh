acd3308d1a04f68fe0f4f2be8b8759fcab9b002b
Tom Salvo <tsalvo@noreply.codeberg.org>
2026-09-06 13:38:22 -0700

    update build scripts for Linux

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-~/intelFPGA_lite/24.1std/quartus/bin/quartus_sh --flow compile ./src/fpga/ap_core.qpf
+~/.local/altera_lite/25.1std/quartus/bin/quartus_sh --flow compile ./src/fpga/ap_core.qpf
 rbfrmake ./src/fpga/output_files/ap_core.rbf ./src/fpga/output_files/bitstream.rbf_r
--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-~/intelFPGA_lite/24.1std/quartus/bin/quartus_sh --flow clean src/fpga/ap_core.qpf
+~/.local/altera_lite/25.1std/quartus/bin/quartus_sh --flow clean src/fpga/ap_core.qpf
